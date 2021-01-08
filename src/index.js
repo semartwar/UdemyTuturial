927a197283e1f1bb529784de740e4e4dc6757cd2
semartwar <ros-gel-dor@yandex.ru>
2021-01-08 14:11:07 +0500

    More variabels

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name = "Sam";
+const fName = "Sam";
+const lName = "Mal";
 const luckyNumber = 7;
 
 ReactDOM.render(
   <div>
-    <h1>Hello {name} !</h1>
+    <h1>
+      Hello {fName} {lName}!
+    </h1>
     <p>Your lucky number is {luckyNumber}</p>
   </div>,
   document.getElementById("root")
