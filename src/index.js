b6a5c383a888088fc18cd1cf056cc9e1d5e54e46
semartwar <ros-gel-dor@yandex.ru>
2021-01-08 14:01:23 +0500

    Deleted comment

--- src/index.js
+++ src/index.js
@@ -11,8 +11,3 @@ ReactDOM.render(
   </div>,
   document.getElementById("root")
 );
-
-//Create a react app from scratch.
-//It should display a h1 heading.
-//It should display an unordered list (bullet points).
-//It should contain 3 list elements.
