bc36b9dc0deff52dabc4586397598522b8a63e44
semartwar <ros-gel-dor@yandex.ru>
2021-01-10 20:04:52 +0500

    The H1 depends on the time of day

--- src/index.js
+++ src/index.js
@@ -3,9 +3,33 @@ import ReactDOM from "react-dom";
 
 const urlImage = "https://picsum.photos/200";
 
+const customStyle = {
+  color: "green",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
+const date = new Date(2013, 2, 1, 1, 13);
+const time = date.getHours();
+let heading = "Good Morning";
+
+if (time >= 12 && time <= 18) {
+  heading = "Good Afternoon";
+  customStyle.color = "green";
+} else if (time >= 18 && time <= 24) {
+  heading = "Good Evening";
+  customStyle.color = "blue";
+} else {
+  heading = "Good Morning";
+  customStyle.color = "red";
+}
+
+// customStyle.color = "purple";
+
 ReactDOM.render(
   <div>
-    <h1 className="heading">My Favourite Foods</h1>
+    <h1 style={customStyle}>{heading}</h1>
+    <p>bla bla</p>
     <div>
       <img className="image" src={urlImage} alt="" />
       <img className="image" src={urlImage} alt="" />
